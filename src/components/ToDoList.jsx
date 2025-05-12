442f3671a5bfe99c36620619e345293874168c5b
Darren-Holmes <dholmes5247@hotmail.com>
2025-05-12 15:58:09 -0500

    sdfdfdsaa

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,16 @@
+const ToDoList = () => {
+    const tasks = ["Learn React", "Build a Project", "Read Documentation"]
+
+    return (
+        <div>
+            <ul>
+                <li>"Learn React"</li>
+                <li>Build a Project</li>
+                <li>Read Documentation</li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default ToDoList;
